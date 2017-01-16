2b55b0916347a1214512e7ae07a5920359a64599
Uk-Jin <ukjin1192@gmail.com>
2017-01-16 13:23:39 +0900

    Update usage of celery task

--- mysite/apps/utils/cron.py
+++ mysite/apps/utils/cron.py
@@ -4,8 +4,20 @@
 from celery import task
 
 @task()
-def do_some_async_task():
+def do_some_async_task(var_1, var_2, *args, **kwargs):
     """
     Do some async task via Celery
+    Usage:
+        do_some_async_task.apply_async(
+            args=[
+                'variable 1',
+                'variable 2',
+                'arguments 1', 'arguments 2', 'arguments 3'
+            ],
+            kwargs={
+                'kwargs_1': 'foo', 
+                'kwargs_2': 'bar'
+            }
+        )
     """
     return None
